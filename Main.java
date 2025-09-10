226f0baeaa9670fe9a642aff9f7111cc5a46df62
leonardo moretti <leonardonabarro2003@gmail.com>
2025-09-10 17:42:28 -0300

    refact: refacting client entitie and main tests

--- Main.java
+++ Main.java
@@ -2,18 +2,43 @@ import Entities.Cliente;
 import Entities.Produto;
 
 public class Main {
-
     public static void main(String[] args) {
-        Cliente c = new Cliente("Leoanrdoi", "leonardo@gmail.conm", 1000);
-        Produto iphone = new Produto("Iphone", 600);
-        Produto mesa = new Produto("Mesa", 600);
+        // Criar produtos
+        Produto p1 = new Produto("Notebook", 3000);
+        Produto p2 = new Produto("Mouse Gamer", 150);
+        Produto p3 = new Produto("Teclado Mecânico", 350);
 
-        c.adicionarProdutoAoCarrinho(iphone);
-        c.adicionarProdutoAoCarrinho(mesa);
-        c.mostrarProdutosNoCarrinho();
-        c.realizarPedido();
-        c.realizarPagamentoDoPedido();
-        c.fazerDeposito(1000);
-        c.realizarPagamentoDoPedido();
+        // Criar cliente com saldo inicial
+        Cliente cliente = new Cliente("Leonardo", "leo@email.com", 500);
+        cliente.exibirInformacoes();
+
+        // Cliente faz depósito
+        cliente.fazerDeposito(3000);
+        cliente.exibirInformacoes();
+
+        // Adicionar produtos no carrinho
+        cliente.adicionarProdutoAoCarrinho(p1);
+        cliente.adicionarProdutoAoCarrinho(p2);
+        cliente.adicionarProdutoAoCarrinho(p3);
+
+        // Mostrar carrinho
+        System.out.println("\n--- Produtos no carrinho ---");
+        cliente.mostrarProdutosNoCarrinho();
+
+        // Realizar pedido
+        System.out.println("\n--- Realizando pedido ---");
+        cliente.realizarPedido();
+
+        // Realizar pagamento
+        System.out.println("\n--- Pagando pedido ---");
+        cliente.realizarPagamentoDoPedido();
+
+        // Tentar pagar de novo (tratamento de já pago)
+        System.out.println("\n--- Tentando pagar novamente ---");
+        cliente.realizarPagamentoDoPedido();
+
+        // Exibir informações finais do cliente
+        System.out.println("\n--- Informações do cliente ---");
+        cliente.exibirInformacoes();
     }
 }
