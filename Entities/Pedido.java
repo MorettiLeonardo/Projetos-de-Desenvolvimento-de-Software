e6f109f8353b9272fd4880a541ee98e95429e3c4
leonardo moretti <leonardonabarro2003@gmail.com>
2025-09-10 18:04:19 -0300

    feat: create pagamento eentity

--- Entities/Pedido.java
+++ Entities/Pedido.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.UUID;
 
 import Enums.STATUS_PEDIDO;
+import Enums.STATUS_PAGAMENTO;
 
 public class Pedido {
     private final String id = UUID.randomUUID().toString();
@@ -12,6 +13,7 @@ public class Pedido {
     private STATUS_PEDIDO statusPedido = STATUS_PEDIDO.PENDENTE;
     private float valorDoPedido = 0;
     private boolean pedidoPago = false;
+    private Pagamento pagamento;
 
     public String getId() {
         return id;
@@ -31,6 +33,10 @@ public class Pedido {
 
     public boolean isPedidoPago() {
         return pedidoPago;
+    }
+
+    public Pagamento getPagamento() {
+        return pagamento;
     }
 
     public void realizarPedido(List<Produto> carrinho) {
@@ -55,12 +61,22 @@ public class Pedido {
         }
 
         if (this.statusPedido == STATUS_PEDIDO.CANCELADO) {
-            System.out.println("Pedido cancelado refaça o pedido novamente.");
+            System.out.println("Pedido cancelado, refaça o pedido novamente.");
             return;
         }
 
-        this.pedidoPago = true;
-        this.statusPedido = STATUS_PEDIDO.CONCLUIDO;
-        System.out.println("Pagamento realizado com sucesso!");
+        if (this.pedidoPago) {
+            System.out.println("Este pedido já foi pago.");
+            return;
+        }
+
+        this.pagamento = new Pagamento(this.valorDoPedido);
+        this.pagamento.confirmarPagamento();
+
+        if (this.pagamento.getStatus() == STATUS_PAGAMENTO.CONCLUIDO) {
+            this.pedidoPago = true;
+            this.statusPedido = STATUS_PEDIDO.CONCLUIDO;
+            System.out.println("Pagamento realizado com sucesso!");
+        }
     }
 }
